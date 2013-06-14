f73e7a0ce36f719da8d2889c3743f24e2385b450
Tyler Shipe <tjshipe@gmail.com>
2013-06-14 13:38:35 -0500

    Add card class

---
+++ card.rb
@@ -0,0 +1,14 @@
+class Card
+  attr_reader :description, :word
+
+  def initialize(arrgs = {})
+    @description = arrgs[:description]
+    @word = arrgs[:word]
+  end
+
+  def to_s
+    "#{description}"
+    "#{word}"
+  end
+end
+
