903c2ccc75f3038f287ba7782f0fd810d8b3c779
Tyler Shipe <tjshipe@gmail.com>
2013-06-14 16:42:49 -0500

    Update card count

--- card.rb
+++ card.rb
@@ -1,14 +1,26 @@
 class Card
-  attr_reader :description, :word
+  attr_reader :description, :word, :guess_count, :display_count
 
   def initialize(arrgs = {})
     @description = arrgs[:description]
-    @word = arrgs[:word]
-    # @count = arrgs[:count]
+    @word = arrgs[:word].strip
+    @display_count = 0
+    @guess_count = 0
   end
 
   def to_s
     "#{description} #{word}"
   end
-end
 
+  def increase_display_count
+    @display_count += 1
+  end
+
+  def increase_guess_count
+    @guess_count += 1
+  end  
+
+  def reset_guess_count
+    @guess_count = 0
+  end
+end
